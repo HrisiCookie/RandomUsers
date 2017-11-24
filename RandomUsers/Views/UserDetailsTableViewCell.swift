c9edb0f1b844c11224c6825ba678b5a1272b5d1d
Hristina Bailova <hristina.bailova@gmail.com>
2017-11-24 04:01:46 +0200

    fixed response

---
+++ RandomUsers/Views/UserDetailsTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  UserDetailsTableViewCell.swift
+//  RandomUsers
+//
+//  Created by Hristina Bailova on 11/24/17.
+//  Copyright © 2017 Hristina Bailova. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userInfo: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func populate(userInfo: String) {
+        self.userInfo.text = userInfo
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
